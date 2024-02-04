50b6fc16f33bacc52a411017f95c165da25a4b92
Melbin Mathew <130178579+MelbinMathew710@users.noreply.github.com>
2024-02-04 16:53:01 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,7 @@
+function showAddCustomerForm() {
+  document.getElementById("addCustomerForm").style.display = "block";
+}
+
+function searchCustomers() {
+  // Implement search functionality here
+}
